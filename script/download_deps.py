6b74c4b12fdafe3d3ab1790ae0ee9b30d1d4b075
Olanti <olanti-p@yandex.ru>
2023-08-09 19:06:40 +0300

    Download dependencies via script

---
+++ script/download_deps.py
@@ -0,0 +1,7 @@
+import urllib.request
+
+urllib.request.urlretrieve("https://github.com/yt-dlp/yt-dlp/releases/download/2023.07.06/yt-dlp.exe", "yt-dlp.exe")
+
+urllib.request.urlretrieve("https://github.com/olanti-p/phantomjs/releases/download/2.1.1/phantomjs.exe", "phantomjs.exe")
+
+urllib.request.urlretrieve("https://github.com/olanti-p/atomicparsley/releases/download/tag-1/AtomicParsley.exe", "AtomicParsley.exe")
